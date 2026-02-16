7728738d45f4b418838af1a5c50e54bc36dc5751
eric-de-lucas <ericdelucass@gmail.com>
2026-02-16 19:49:24 -0300

    asd

---
+++ a.py
@@ -0,0 +1,11 @@
+
+import pyautogui
+import time
+
+for i in range(16):
+    pyautogui.click(x=3111, y=117)
+    time.sleep(1)
+    pyautogui.click(x=3005, y=205)
+    time.sleep(2)
+    pyautogui.click(x=3126, y=527)
+    time.sleep(2)
